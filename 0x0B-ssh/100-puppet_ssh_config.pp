05418573613888750d87601723884ae505bd5d80
AzorAha1 <wizfaiz@icloud.com>
2024-01-02 09:29:27 +0100

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,10 @@
 # using puppet to make changes to my configuration file
 include stdlib
-file_line {'turn off password authentication':
+file_line {'turn off password auth':
   line => 'PasswordAuthentication no',
   path => '/home/vboxuser/.ssh/config',
 }
-file_line{'file that contains the priv key':
+file_line{'Declare identity file':
   line => 'IdentityFile',
   path => '/home/vboxuser/.ssh/config',
 }
-
