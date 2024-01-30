079ebc4d20263eecef90a36584b6801b63d77a36
AzorAha1 <wizfaiz@icloud.com>
2024-01-30 01:35:48 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """gather data from an api"""
 import requests, urllib.request
-import json
+import json, sys
 
-url='https://jsonplaceholder.typicode.com/todos/1'
+employee_id = int(sys.argv[1])
+url=f'https://jsonplaceholder.typicode.com/todos/{employee_id}'
 with urllib.request.urlopen(url=url) as request:
     data = json.load(request)
     employee_name = data['EMPLOYEE_NAME']
     number_of_done_tasks = data['NUMBER_OF_DONE_TASKS']
     total_number_of_tasks = data['TOTAL_NUMBER_OF_TASKS']
-    task_title = data['TASK_TITLE']
-    output = f'employee {employee_name}'
+    task_title = data['title']
+    output = f'employee {task_title}'
     print(output)
