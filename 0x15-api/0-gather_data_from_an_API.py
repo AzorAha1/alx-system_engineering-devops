27028487d294ed79c191fb1ba2f6fad44659e993
AzorAha1 <wizfaiz@icloud.com>
2024-01-30 03:06:33 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,20 +5,20 @@ import sys
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
-    name_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    response_name = requests.get(url=name_url)
-    response_todo = requests.get(url=todos_url)
+    nm_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    td_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    response_name = requests.get(url=nm_url)
+    response_todo = requests.get(url=td_url)
     data_name = response_name.json()
     data_todo = response_todo.json()
-    employee_name = data_name['name']
-    completed_task_sum = 0
+    empy_name = data_name['name']
+    comptasksum = 0
     all_task_sum = 0
     for data in data_todo:
         if data['completed'] is True:
-            completed_task_sum += 1
+            comptasksum += 1
         all_task_sum += 1
-    print(f'Employee {employee_name}\
-            is done tasks({completed_task_sum}/{all_task_sum}):')
+    print(f'Employee {empy_name} is done tasks({comptasksum}/{all_task_sum}):')
     for data in data_todo:
-        print(f'\t {data["title"]}')
+        if data['completed'] is True:
+            print(f'\t {data["title"]}')
