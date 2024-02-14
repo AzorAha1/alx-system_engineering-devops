94c095bf4239d4a0594dc9f267d3fcb59b9852f8
AzorAha1 <wizfaiz@icloud.com>
2024-02-15 00:37:06 +0100

    check if data is in response body and if subscribe is hte the data body part

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,5 +17,5 @@ def number_of_subscribers(subreddit):
             'data' in resget.json() and 'subscribers' in resget.json()['data']:
         sub = resget.json().get('data', {}).get('subscribers')
         return (sub)
-    else:
+    elif resget.status_code == 404:
         return (0)
