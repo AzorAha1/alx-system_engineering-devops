d55ead7b364f71ced886bf94d674ae0507f2d261
AzorAha1 <wizfaiz@icloud.com>
2024-02-14 02:45:15 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,16 +5,17 @@
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """number of subs"""
+def top_ten(subreddit):
+    """top ten posts in a subreddit"""
     client_id = 'fsyHwM_7gSQI9-Jn3lpwOQ'
     secret_key = '1LOwBUH-z-_f8LcDIJPenc3GBdPGrQ'
     auth = requests.auth.HTTPBasicAuth(client_id, secret_key)
     headers = {'User-Agent': 'faisalapi/0.0.1'}
-    url = f'https://oauth.reddit.com/r/{subreddit}/about.json'
+    url = f'https://oauth.reddit.com/r/{subreddit}/top.json?t=day&limit=10'
     resget = requests.get(url=url, auth=auth, headers=headers)
     if resget.status_code == 200:
-        sub = resget.json().get('data', {}).get('subscribers')
-        return (sub)
+        subs = resget.json().get('data', {}).get('children')
+        for sub in subs:
+            print(sub.get('data', {}).get('title'))
     else:
-        return (0)
+        print(None)
