b8705e2349c57f247081f928a47baaf2435b5009
AzorAha1 <wizfaiz@icloud.com>
2024-02-14 01:14:56 +0100

    update

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """number of subs"""
+    client_id = 'fsyHwM_7gSQI9-Jn3lpwOQ'
+    secret_key = '1LOwBUH-z-_f8LcDIJPenc3GBdPGrQ'
+    auth = requests.auth.HTTPBasicAuth(client_id, secret_key)
+    headers = {'User-Agent': 'faisalapi/0.0.1'}
+    url = f'https://oauth.reddit.com/r/{subreddit}/about.json'
+    resget = requests.get(url=url, auth=auth, headers=headers)
+    if resget.status_code == 200:
+        sub = resget.json().get('data', {}).get('subscribers')
+        return (sub)
+    else:
+        return (0)
