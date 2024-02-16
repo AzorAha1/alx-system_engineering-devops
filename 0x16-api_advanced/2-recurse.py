6a60e0d5bfe90f069b908f126245b35deedeb54a
AzorAha1 <wizfaiz@icloud.com>
2024-02-16 01:19:17 +0100

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,14 +3,12 @@
 
 
 import requests
-import sys
 
 
 def recurse(subreddit, hot_list=[], after=None):
     """recursive function returns list containing title of all hot articles"""
     client_id = 'fsyHwM_7gSQI9-Jn3lpwOQ'
     secret_key = '1LOwBUH-z-_f8LcDIJPenc3GBdPGrQ'
-    subreddit = sys.argv[1]
     params = {
         't': 'day',
         'after': after
@@ -20,12 +18,12 @@ def recurse(subreddit, hot_list=[], after=None):
     url = f'https://oauth.reddit.com/r/{subreddit}/hot.json'
     resget = requests.get(auth=auth, headers=headers, url=url, params=params)
     if resget.status_code == 200:
-        previous = resget.json()['data']['after']
         listofitems = resget.json()['data']['children']
         for item in listofitems:
             hot_list.append(item['data']['title'])
+        previous = resget.json()['data']['after']
         if previous is not None:
-            recurse(subreddit=subreddit, hot_list=hot_list, after=previous)
+            recurse(subreddit, hot_list, previous)
         return hot_list
     else:
         return None
