52f2fe4d075765abbaa97b4d7c33facb3db2d493
AzorAha1 <wizfaiz@icloud.com>
2024-02-22 21:17:31 +0100

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,11 @@
 file { '/etc/default/nginx'
   ensure  => file,
   content => template('nginx/nginx_ulimit.erb'),
+  notify  => Exec['nginx restart']
 }
 
 exec { 'nginx restart':
   command => 'systemctl restart nginx',
+  require => File['/etc/default/nginx'],
+  path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin']
 }
