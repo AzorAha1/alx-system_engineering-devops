50da318aab7c402cbd1496be1d50b07547648b4f
AzorAha1 <wizfaiz@icloud.com>
2024-02-24 12:18:42 +0100

    update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,17 @@
 #adding new user
 
+#user
 user { 'holberton':
   ensure   => present,
   password => 'changeme',
-  # shell    => '/bin/bash',
-  # uid      => '501',
-  # gid      => '20',
 }
+
+#ulimit increase
+exec { 'name':
+  command => '/bin/echo "holberton soft nofile  1048576" >> /etc/security/limits.conf && ' +
+            '/bin/echo "holberton hard nofile  1048578" >> /etc/security/limits.conf',
+}
+
 
 exec { 'permission':
   command => 'usermod -aG sudo holberton',
